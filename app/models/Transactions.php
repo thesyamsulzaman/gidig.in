0fc07cc40e08d0a51b7656ebf620cf343abe3642
Syamsul Zaman <thesyamsulzaman@gmail.com>
2021-02-17 07:13:00 +0700

    Adding the Transactions Table

--- app/models/Transactions.php
+++ app/models/Transactions.php
@@ -5,9 +5,13 @@ namespace App\Models;
 use Core\Model;
 use Core\Helpers;
 
-use Core\Validators\;
+use Core\Validators\RequiredValidator;
 
 class Transactions extends Model {
+	protected static $_table = "transactions";
+	protected static $_softDelete = true;
+
+	public $id, $created_at, $update_at;
 
 }
 
